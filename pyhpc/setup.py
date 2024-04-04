d7ee6368933d3f8eb7784754402e9f268fa9ed5e
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-04-03 23:42:59 -0400

    added setup.py

---
+++ pyhpc/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pyhpc",
+    description="HPC Python Helper",
+    version="0.0.1.dev1",
+    url="https://github.com/jdenhof/pygit",
+    author="Jagger Denhof",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    # extras_require={
+    #     "test": [
+    #         "pytest",
+    #         "pytest-cov"
+    #     ]
+    # }
+)
